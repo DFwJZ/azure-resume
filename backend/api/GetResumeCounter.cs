f6c26d3a0c0ff6c39427f40329f44683b112340c
HaozheZhang0818 <zhang.haozhe1@northeastern.edu>
2023-08-24 21:18:40 -0700

    test backend workflow01

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -30,6 +30,7 @@ namespace Company.Function
                 PartitionKey ="1")] out Counter updatedCounter,
             ILogger log)
         {
+            // Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = null;
